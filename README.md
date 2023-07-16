de30070b16a97f7de02ed829fd46ef50b29100fc
Trevor <trevorward001@gmail.com>
2023-07-16 14:41:51 -0600

    fixed NextLink issue, as it is now treated as <a> itself

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Personal-Website
+
+This is my personal website I've taken from this [youtube video](https://www.youtube.com/watch?v=bSMZgXzC9AA&t=1278s).
--- components/navbar.js
+++ components/navbar.js
@@ -1,5 +1,6 @@
 import Logo from './logo'
 import NextLink from 'next/link'
+
 import {
     Container,
     Box,
@@ -20,26 +21,25 @@ const LinkItem = ({href, path, children}) => {
     const active = path === href
     const inactiveColor = useColorModeValue('gray200','whiteAlpha.900')
     return (
-        <NextLink href={href}>
-           <Link
+        <NextLink href={href}
            p={2}
            bg={active ? 'glassTeal' : undefined}
-           color={active ? '#202023': inactiveColor}>
+           color={active ? '#202023': inactiveColor}
+           >
             {children}
-           </Link> 
         </NextLink>
     )
 }
 
 const Navbar = props => {
-    const {path} = props
+    const { path } = props
 
     return (
         <Box
             position="fixed"
             as="nav"
             w="100%"
-            bg={useColorModeValue('#ffffff40','#202023')}
+            bg={useColorModeValue('#ffffff40','#20202380')}
             style={{backdropFilter:'blur(10px)'}}
             zIndex={1}
             {...props}
@@ -66,8 +66,10 @@ const Navbar = props => {
             flexGrow={ 1}
             mt={{base: 4, nmd: 0}}
             >
+                <LinkItem href='/works' path={path}>
+                Works
+                </LinkItem>
             </Stack>
-        {/* <LinkItem href="" path={path}>Works</LinkItem> */}
         </Container>
         </Box>
     )
