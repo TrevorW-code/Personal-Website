347f84e0285b072375922048aed35fae63292c35
Trevor <trevorward001@gmail.com>
2024-04-17 17:09:20 -0600

    feat: working blog route
    - just need to update styles
    - might add a macro for rendering face and video, have some cool ideas

---
+++ src/blog/test.md
@@ -0,0 +1,8 @@
+---
+layout: 'layouts/post.njk'
+title: Woah! 
+subtitle: First Post! 
+tags: post
+---
+
+This is totally a test
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["_site/**/*.{html,njk}"],
-  theme: {
-    extend: {
-      theme1: '#30b1ce',
-      theme2: '#29a4c3',
-      theme3: '#318db2',
-      theme4: '#046997',
-      theme5: '#003b61'
-    },
-  plugins: [],
+	content: ["_site/**/*.{html,njk}"],
+	theme: {
+	extend: {
+		colors: {
+			theme: {
+					1: '#30b1ce',
+					2: '#29a4c3',
+					3: '#318db2',
+					4: '#046997',
+					5: '#003b61',
+			}
+		}
+	},
+	plugins: [],
+	}
 }
-  }
