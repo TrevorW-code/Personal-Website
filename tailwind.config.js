6696c4ff2d40f19b5901d169e53b2687e82cd52f
Trevor <trevorward001@gmail.com>
2024-04-16 22:00:03 -0600

    started themes

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,13 @@
 module.exports = {
   content: ["_site/**/*.{html,njk}"],
   theme: {
-    extend: {},
-  },
+    extend: {
+      theme1: '#30b1ce',
+      theme2: '#29a4c3',
+      theme3: '#318db2',
+      theme4: '#046997',
+      theme5: '#003b61'
+    },
   plugins: [],
 }
-
+  }
